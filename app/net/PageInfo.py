04eaeb77cc5a1a74de37a7de2f2e079ede888486
Javinator9889 <javialonso007@hotmail.es>
2018-05-03 23:55:18 +0200

    Created constructor

--- app/net/PageInfo.py
+++ app/net/PageInfo.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class Connection:
+    KERNEL_PAGE = "https://www.kernel.org/"
+
+    def __init__(self):
+        saved_page_content = requests.get(self.KERNEL_PAGE).content
+        self.soupObject = BeautifulSoup(saved_page_content, "lxml")
 
