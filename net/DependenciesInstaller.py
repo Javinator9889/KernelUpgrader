fe52d053e7750d6aa99c723fe2fc4386ed6e3b36
Javinator9889 <javialonso007@hotmail.es>
2018-05-06 18:15:26 +0200

    TypeError - Log() is not callable solved

--- net/DependenciesInstaller.py
+++ net/DependenciesInstaller.py
@@ -1,21 +1,18 @@
-from utils import Log
-
-
 class Dependencies:
-    def __init__(self):
-        self.__log = Log.instance()
-
-    def installRequiredDependencies(self):
+    @staticmethod
+    def installRequiredDependencies():
         import subprocess
         from values.Constants import DEPENDENCIES
         from exceptions import UnableToInstallDependencies
-
+        # from Application import __log
+        from utils import Log
+        __log = Log.instance()
         process = subprocess.run(DEPENDENCIES.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if process.returncode != 0:
-            self.__log().e("Impossible to install dependencies. Error output: " + process.stderr.decode("utf-8"))
-            self.__log().finish()
+            __log.e("Impossible to install dependencies. Error output: " + process.stderr.decode("utf-8"))
+            __log.finish()
             raise UnableToInstallDependencies("There was a problem while trying to install required dependencies."
                                               " Please, install them manually: " + DEPENDENCIES + "\nError output: " +
                                               process.stderr.decode("utf-8"))
         else:
-            self.__log().i("Dependencies installation finished")
+            __log.i("Dependencies installation finished")
