41d097871a6313ce6708905ba14e8a175b6ded9b
Javinator9889 <javialonso007@hotmail.es>
2018-05-06 18:06:04 +0200

    Clint for download progress

--- net/Downloader.py
+++ net/Downloader.py
@@ -21,35 +21,46 @@ class Downloader:
 
     def startDownload(self):
         # type: () -> (str, str)
-        from urllib.parse import urlparse
-        import os
-        import sys
-        import requests
-        path = urlparse(self.__url).path
-        filename = os.path.basename(path)
-        partial_path = self.__HOME + "/Downloads/linux_{}_{}/".format(self.__version, self.__date)
-        if not os.path.exists(partial_path):
-            os.makedirs(partial_path)
-        download_path = partial_path + filename
-        self.__log.d("Downloading to: " + download_path)
-        with open(download_path, "wb") as download:
-            response = requests.get(self.__url, stream=True)
-            total_length = response.headers.get(DOWNLOAD_LENGTH)
-            if total_length is None:
-                download.close()
-                self.__log.e("The kernel is not available actually for download")
-                raiserContentNotAvailable(response.content)
-            else:
-                bytes_downloaded = 0
-                total_length = int(total_length)
-                for data_obtained in response.iter_content(chunk_size=4096):
-                    bytes_downloaded += len(data_obtained)
-                    download.write(data_obtained)
-                    completed = int(50 * bytes_downloaded / total_length)
-                    sys.stdout.__write("\r[%s%s]" % ('=' * completed, ' ' * (50 - completed)))
-                    sys.stdout.flush()
-                download.flush()
-                download.close()
-                length_in_mb = total_length / 1000000
-                self.__log.i("Downloaded " + str(total_length) + " bytes (" + str("%.2f" % length_in_mb) + " MB)")
-        return download_path, self.__date
+        try:
+            from urllib.parse import urlparse
+            from clint.textui import progress
+            import os
+            # import sys
+            import requests
+            path = urlparse(self.__url).path
+            filename = os.path.basename(path)
+            partial_path = self.__HOME + "/Downloads/linux_{}_{}/".format(self.__version, self.__date)
+            if not os.path.exists(partial_path):
+                os.makedirs(partial_path)
+            download_path = partial_path + filename
+            self.__log.d("Downloading to: " + download_path)
+            with open(download_path, "wb") as download:
+                response = requests.get(self.__url, stream=True)
+                total_length = response.headers.get(DOWNLOAD_LENGTH)
+                if total_length is None:
+                    download.close()
+                    self.__log.e("The kernel is not available actually for download")
+                    raiserContentNotAvailable(response.content)
+                else:
+                    """bytes_downloaded = 0
+                    total_length = int(total_length)
+                    for data_obtained in response.iter_content(chunk_size=4096):
+                        bytes_downloaded += len(data_obtained)
+                        download.write(data_obtained)
+                        completed = int(50 * bytes_downloaded / total_length)
+                        sys.stdout.__write("\r[%s%s]" % ('=' * completed, ' ' * (50 - completed)))
+                        sys.stdout.flush()
+                    download.flush()
+                    download.close()
+                    length_in_mb = total_length / 1000000"""
+                    for chunk in progress.bar(response.iter_content(chunk_size=1024),
+                                              expected_size=int(total_length / 1024) + 1):
+                        if chunk:
+                            download.write(chunk)
+                            download.flush()
+                    download.close()
+                    length_in_mb = total_length / 1000000
+                    self.__log.i("Downloaded " + str(total_length) + " bytes (" + str("%.2f" % length_in_mb) + " MB)")
+            return download_path, self.__date
+        except ImportError as e:
+            raiserModuleNotFound(e)
