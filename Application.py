dab79e60873a4b7abae60f542a0194fbe8f4898e
Javinator9889 <javialonso007@hotmail.es>
2018-05-06 17:37:30 +0200

    Packaging is an extra module

--- Application.py
+++ Application.py
@@ -3,7 +3,7 @@ import argparse
 from utils import isRunningLinux, Log, isUserAdmin, getLinuxVersion
 from utils.colors import OutputColors as Colors
 from values.Constants import REPO_URL, FILE_PATH, FILENAME, COMPILER_FILENAME
-from exceptions import LinuxSystemNotFound, RootPrivilegesNotGiven
+from exceptions import LinuxSystemNotFound, RootPrivilegesNotGiven, raiserModuleNotFound
 from net.PageInfo import Connection
 from net.Downloader import Downloader
 from net.DependenciesInstaller import Dependencies
@@ -49,50 +49,53 @@ def main(arg):
                 current_version = getLinuxVersion()
                 info = Connection()
                 new_version = info.getLatestVersionCode()
-                from packaging import version
-                if version.parse(current_version) >= version.parse(new_version):
-                    __log.d("The version installed is the same or greater than the available one. Current version: " +
-                            current_version + " | Available version: " + new_version)
-                    print(Colors.WARNING + "You already have the latest version" + Colors.ENDC)
-                    exit(1)
-                else:
-                    print(Colors.OKBLUE + "Downloading new version... " + Colors.ENDC + "| New version: " + new_version)
-                    __log.d("Starting new version download... | New version: " + new_version)
-                    version_url = info.getLatestVersionURL()
-                    downloader = Downloader(version_url, new_version)
-                    download_path, current_date = downloader.startDownload()
-                    __log.d("Starting dependencies installation...")
-                    print(Colors.OKBLUE + "Installing required dependencies... " + Colors.ENDC)
-                    Dependencies.installRequiredDependencies()
-                    __log.d("Starting kernel decompression")
-                    print(Colors.OKBLUE + "Decompressing downloaded kernel..." + Colors.ENDC)
-                    unzipper = UnZipper(download_path)
-                    kernel_folder = unzipper.unzip()
-                    __log.d("Finished kernel decompression")
-                    __log.d("Starting kernel compilation...")
-                    print(Colors.OKBLUE + "Copying old configuration..." + Colors.ENDC)
-                    compiler = Compiler(kernel_folder, new_version, current_date)
-                    __log.d("Copying old kernel boot config")
-                    if compiler.copy_latest_config():
-                        __log.d("Adapting latest config for the new kernel version")
-                        print(Colors.OKBLUE + "Adapting old configuration to the new kernel..." + Colors.ENDC)
-                        compiler.adaptOldConfig()
-                        __log.d("Performing kernel compilation...")
-                        print(Colors.OKBLUE + "Starting kernel compilation..." + Colors.ENDC)
-                        print(Colors.WARNING + "This process will take a long time to finish. You can do it "
-                                               "in background by pressing \"Ctrl + Z\" and then, type \"bg\" at your"
-                                               " terminal" + Colors.ENDC)
-                        compiler.compileKernel()
-                        __log.d("Kernel compilation finished")
-                        __log.d("Starting kernel installation...")
-                        print(Colors.OKBLUE + "Installing the new kernel..." + Colors.ENDC)
-                        compiler.installKernel()
-                        __log.d("Finished correctly kernel installation. New version installed: " + new_version)
-                        __log.finish()
-                        print(Colors.OKGREEN + "Kernel completely installed. Now you should reboot in order to apply"
-                                               " changes. New version: " + new_version + Colors.ENDC)
-                        exit(0)
-
+                try:
+                    from packaging import version
+                    if version.parse(current_version) >= version.parse(new_version):
+                        __log.d("The version installed is the same or greater than the available one. Current version: " +
+                                current_version + " | Available version: " + new_version)
+                        print(Colors.WARNING + "You already have the latest version" + Colors.ENDC)
+                        exit(1)
+                    else:
+                        print(Colors.OKBLUE + "Downloading new version... " + Colors.ENDC + "| New version: " + new_version)
+                        __log.d("Starting new version download... | New version: " + new_version)
+                        version_url = info.getLatestVersionURL()
+                        downloader = Downloader(version_url, new_version)
+                        download_path, current_date = downloader.startDownload()
+                        __log.d("Starting dependencies installation...")
+                        print(Colors.OKBLUE + "Installing required dependencies... " + Colors.ENDC)
+                        Dependencies.installRequiredDependencies()
+                        __log.d("Starting kernel decompression")
+                        print(Colors.OKBLUE + "Decompressing downloaded kernel..." + Colors.ENDC)
+                        unzipper = UnZipper(download_path)
+                        kernel_folder = unzipper.unzip()
+                        __log.d("Finished kernel decompression")
+                        __log.d("Starting kernel compilation...")
+                        print(Colors.OKBLUE + "Copying old configuration..." + Colors.ENDC)
+                        compiler = Compiler(kernel_folder, new_version, current_date)
+                        __log.d("Copying old kernel boot config")
+                        if compiler.copy_latest_config():
+                            __log.d("Adapting latest config for the new kernel version")
+                            print(Colors.OKBLUE + "Adapting old configuration to the new kernel..." + Colors.ENDC)
+                            compiler.adaptOldConfig()
+                            __log.d("Performing kernel compilation...")
+                            print(Colors.OKBLUE + "Starting kernel compilation..." + Colors.ENDC)
+                            print(Colors.WARNING + "This process will take a long time to finish. You can do it "
+                                                   "in background by pressing \"Ctrl + Z\" and then, type \"bg\" at your"
+                                                   " terminal" + Colors.ENDC)
+                            compiler.compileKernel()
+                            __log.d("Kernel compilation finished")
+                            __log.d("Starting kernel installation...")
+                            print(Colors.OKBLUE + "Installing the new kernel..." + Colors.ENDC)
+                            compiler.installKernel()
+                            __log.d("Finished correctly kernel installation. New version installed: " + new_version)
+                            __log.finish()
+                            print(Colors.OKGREEN + "Kernel completely installed. Now you should reboot in order to apply"
+                                                   " changes. New version: " + new_version + Colors.ENDC)
+                            exit(0)
+                except ImportError as e:
+                    raiserModuleNotFound(e)
+                    
 
 if __name__ == '__main__':
     arguments = argparse.ArgumentParser(prog=__program_name,
