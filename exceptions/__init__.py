2c79d2bef7d15f7dbe61a11015dcc336fdad455e
Javinator9889 <javialonso007@hotmail.es>
2018-05-06 16:56:54 +0200

    Mismatching parenthesis in exceptions

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -13,55 +13,55 @@ class ContentNotAvailable(RuntimeError):
 class ExtractionError(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class CopyConfigError(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class OldConfigAdaptationError(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class CompilationError(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class RPMNotSupported(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class InstallationError(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class LinuxSystemNotFound(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class RootPrivilegesNotGiven(RuntimeError):
     def __init__(self, message=None):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 class UnableToInstallDependencies(RuntimeError):
     def __init__(self, message):
         self.message = message
-        super.__init__(message)
+        super().__init__(message)
 
 
 def raiserModuleNotFound(exception):
