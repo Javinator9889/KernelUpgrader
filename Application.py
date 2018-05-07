b8e042c2f7d51c4c6ac7bd5f5c5f0a97bbf440a0
Javinator9889 <javialonso007@hotmail.es>
2018-05-07 23:34:58 +0200

    Perfectioned Application.py

--- Application.py
+++ Application.py
@@ -125,6 +125,7 @@ def main(arg):
             except ImportError as e:
                 raiserModuleNotFound(e)
             except KeyboardInterrupt:
+                print("\n")
                 print(Colors.FAIL + "User pressed Ctrl + C - stopping..." + Colors.ENDC)
                 __log.e("User pressed keyboard interrupt. Stopping program...")
                 __log.finish()
