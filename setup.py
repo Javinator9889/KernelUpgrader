2ea3a3c284575bad6d1ba4c79a8830e5be9d6b2f
Javinator9889 <javialonso007@hotmail.es>
2018-05-08 10:29:53 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -28,12 +28,5 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Natural Language :: English'
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ]
 )
