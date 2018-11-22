ea4ec1fbe44e22ca993098615bd52434a6a3c1d1
Javinator9889 <javialonso007@hotmail.es>
2018-11-22 10:06:16 +0100

    None error solved

--- kernel_upgrader/net/KernelVersions.py
+++ kernel_upgrader/net/KernelVersions.py
@@ -9,8 +9,9 @@ class KernelVersions(Connection):
         result = []
         for version in versions:
             parts = version.find_all("td")
-            result.append({"release_type": parts[0].get_text(strip=True),
-                           "release_version": parts[1].get_text(strip=True),
-                           "release_date": parts[2].get_text(strip=True),
-                           "release_url": parts[3].find("a").get("href")})
+            if parts[3].find('a') is not None:
+                result.append({"release_type": parts[0].get_text(strip=True),
+                               "release_version": parts[1].get_text(strip=True),
+                               "release_date": parts[2].get_text(strip=True),
+                               "release_url": parts[3].find("a").get("href")})
         return result
